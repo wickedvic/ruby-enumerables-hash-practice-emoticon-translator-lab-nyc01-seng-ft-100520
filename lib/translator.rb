0247f96e0bb392e56445c7fe26bbdd75729b71d2
Vivek Sanghvi <viksipart@gmail.com>
2020-10-01 19:16:59 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -8,7 +8,8 @@ def load_library(file_path)
   
   emojis.each do |key, value|
     new_hash[key] = {} 
-  new_hash[key][:english] = 
+  new_hash[key][:english] = value[0]
+  new_hash[key][:japanese] = value[1]
   end
 end
 
